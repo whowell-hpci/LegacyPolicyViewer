44d5cba9473396b272e57725924f94d74fa02cda
William Howell <whowell@heritagepci.com>
2020-03-24 12:04:49 -0400

    try adding request headers

--- LegacyPolicyViewer-SPA/src/app/_services/policy.service.ts
+++ LegacyPolicyViewer-SPA/src/app/_services/policy.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Policy } from '../_models/policy';
 import { Observable } from 'rxjs';
 
@@ -9,16 +9,39 @@ import { Observable } from 'rxjs';
 })
 export class PolicyService {
   baseUrl = environment.baseUrl;
+  
 
   constructor(private http: HttpClient) { }
 
+  
+
   getPolicyByNumber(id): Observable<Policy> {
-    return this.http.get<Policy>(this.baseUrl + 'policy/' + id);
+    
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST'
+    }
+  
+    const requestOptions = {
+      headers: new HttpHeaders(headerDict)
+    };
+
+    return this.http.get<Policy>(this.baseUrl + 'policy/' + id, requestOptions);
   }
 
   getPolicyByName(id): Observable<Policy> {
     var replacement = id.replace(" ", "%20");
-    return this.http.get<Policy>(this.baseUrl + 'policy/named/' + replacement)
+    const headerDict = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST'
+    }
+  
+    const requestOptions = {
+      headers: new HttpHeaders(headerDict)
+    };
+    return this.http.get<Policy>(this.baseUrl + 'policy/named/' + replacement, requestOptions)
   }
 
 }
